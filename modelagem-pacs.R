06e31cc0ddd7813c91f25462f9aec8223119587e
silveiratcl <silveira.tcl@gmail.com>
2024-02-28 22:41:35 -0300

    working modelling and evaluation

--- modelagem-pacs.R
+++ modelagem-pacs.R
@@ -6,19 +6,22 @@
 ################################################################################
 
 
-## Definindo o diretório
-setwd("C:/Users/Victoria/OneDrive/Documentos/UFSC/IC/modelagem-pacs")
+setwd("C:/Users/silve/OneDrive/Documentos/Academico/POS-DOC_UFSC/@Karon Coral Sol/modelling/modeling_pacs_2024/modeling_pacs")
 
 ## Instalando os pacotes
-install.packages("raster")
-install.packages("sf")
-install.packages("terra")
-install.packages("sp")
-install.packages("gtools")
-install.packages("car")
-install.packages("psych")
-install.packages("mgcv")
-install.packages("biomod2")
+#install.packages("raster")
+#install.packages("sf")
+#install.packages("terra")
+#install.packages("sp")
+#install.packages("gtools")
+#install.packages("car")
+#install.packages("psych")
+#install.packages("mgcv")
+#install.packages("biomod2")
+#install.packages("dismo")
+#install.packages("tidyterra")
+#install.packages("ggtext")
+
 
 ## Carregamento dos pacotes
 library(raster)
@@ -32,40 +35,37 @@ library(mgcv)
 library(biomod2)
 library(carData)
 library(nlme)
+library(dplyr)
+library(tidyr)
+library(dismo)
+library(tidyterra)
+library(ggtext)
 
 ## Carregando os dados de ocorrência e ausência
-dfocc <- read.table("./occ_abs_cs/occ_model.csv", header = T, sep = ";", dec = ",")
+dfocc <- read.table("./occ_abs_cs/occ_abs_model_edited_occ.csv", header = T, sep = ",", dec = ".")
+head(dfocc)
+dfocc <- dfocc[, c(1,2)] 
 head(dfocc)
 
-dfabs <- read.table("./occ_abs_cs/abs_model.csv", header = T, sep = ";", dec = ",")
+dfabs <- read.table("./occ_abs_cs/occ_abs_model_edited_abs.csv", header = T, sep = ",", dec = ".")
+head(dfabs)
+dfabs <- dfabs[, c(1,2)] 
 head(dfabs)
 
-df <- read.table("./occ_abs_cs/occ_abs_model.csv", header = T, sep = ";", dec = ",")
-head(df)
-
-
 ## Carregando as camadas máscaras #uso da funcao vect pq readogr esta ligada ao pct rgdal
-#study_area <- readOGR("./study_area", "study_area")
-#ocean <- readOGR("./study_area", "ocean_study_area")
-#land <- readOGR("./study_area", "land_study_area")
-#study_area = vect("C:/Users/Victoria/OneDrive/Documentos/UFSC/IC/modelagem-pacs/study_area/study_area.shp")
 study_area = vect("./study_area", "study_area")
-ocean = vect("C:/Users/Victoria/OneDrive/Documentos/UFSC/IC/modelagem-pacs/study_area/ocean_study_area.shp")
+ocean = vect("./study_area/ocean_study_area.shp")
 land = vect("./study_area", "land_study_area")
 
 ## Carregando as variáveis
-#bat <- raster("./variables/bat_resampled.tif")
-#velc <- raster("./variables/velc_null.tif")
-#sst <- raster("./variables/sst_null.tif")
-#d_cost <- raster("./variables/d_cost_resampled.tif")
-#d_mar <- raster("./variables/d_mar_resampled.tif")
-#d_traf <- raster("./variables/d_traf_resampled.tif")
-bat = rast("C:/Users/Victoria/OneDrive/Documentos/UFSC/IC/modelagem-pacs/camada batimetria/bat_resampled.tif")
-velc = rast("C:/Users/Victoria/OneDrive/Documentos/UFSC/IC/modelagem-pacs/camada Velocidade de Corrente/velc_null.tif")
-sst = rast("C:/Users/Victoria/OneDrive/Documentos/UFSC/IC/modelagem-pacs/camada SST/sst_null.tif")
-d_cost = rast("C:/Users/Victoria/OneDrive/Documentos/UFSC/IC/modelagem-pacs/camada Costão/d_cost_resampled.tif")
-d_mar = rast("C:/Users/Victoria/OneDrive/Documentos/UFSC/IC/modelagem-pacs/Portos e Marinas/d_mar_resampled.tif")
-d_traf = rast("C:/Users/Victoria/OneDrive/Documentos/UFSC/IC/modelagem-pacs/camada Tráfego Marinho/d_traf_resampled.tif")
+bat <- raster("./layers/bat_resampled.tif")
+velc <- raster("./layers/velc_resampled.tif")
+sst <- raster("./layers/sst_resampled.tif")
+d_cost <- raster("./layers/d_cost_resampled.tif")
+d_mar <- raster("./layers/d_mar_resampled.tif")
+d_traf <- raster("./layers/d_traf_resampled.tif")
+mhw <- raster("./layers/mhw_resampled.tif")
+mcs <- raster("./layers/mcs_resampled.tif")
 
 ## Filtro de proximidade
 filterByProximity <- function(xy, dist, mapUnits = F) {
@@ -129,21 +129,22 @@ head(df)
 
 ## Criando um stack, ou seja, uma coleção de camadas raster, para as variáveis
 # Stack - empilhamento
-variables <- c(bat, velc, sst, d_cost, d_mar, d_traf)
+variables <- c(bat, velc, sst, d_cost, d_mar, d_traf, mhw, mcs)
 variables <- stack(variables)
-names(variables) <- c ('bat', 'velc', 'sst', 'd_cost', 'd_mar', 'd_traf')
+names(variables) <- c ('bat', 'velc', 'sst', 'd_cost', 'd_mar', 'd_traf', 'mhw', 'mcs')
 variables
-
+str(variables)
 
 ## Extraindo os valores das camadas
 # valor de cada variavel pra cada coordenada
+.rs.unloadPackage("tidyr") #converge com a função extract ---- não funcionou??
 occvals <- extract(variables, dfocc)
 absvals <- extract(variables, dfabs)
 pb <- c(rep(1, nrow(occvals)), rep(0, nrow(absvals)))
 sdmdata <- data.frame(cbind(pb, rbind(occvals, absvals)))
 head(sdmdata)
 tail(sdmdata)
-summary(sdmdata)
+summary(sdmdata) 
 
 saveRDS(sdmdata, "./occ_abs_cs/sdm.Rds")
 saveRDS(occvals, "./occ_abs_cs/occvals.Rds")
@@ -152,10 +153,10 @@ saveRDS(absvals, "./occ_abs_cs/absvals.Rds")
 
 ## Examinando as correlações entre as variáveis
 myData <- sdmdata
-describe(myData[,c(2:7)])
+describe(myData[,c(2:9)])
 
-pairs.panels(myData[,c(2:7)],pch='.')
-lowerCor(myData[,c(2:7)])
+pairs.panels(myData[,c(2:9)],pch='.')
+lowerCor(myData[,c(2:9)])
 dev.off()
 
 
@@ -164,17 +165,12 @@ myRespName <- 'Tubastraea coccinea'
 DataSpecies <- as.numeric(df$occ_abs)
 myRespXY <- df[,c("lon_dd", "lat_dd")]
 
-predictors1 <- variables
-names(predictors1) <- c ('bat', 'velc', 'sst', 'd_cost', 'd_mar', 'd_traf')
+
+variables
+predictors1 <- stack(c(variables@layers[[6]], variables@layers[[7]]))  # 4,6,8
+names(predictors1) <- c ( 'd_traf', 'mhw')
 predictors1
 
-predictors2 <- stack(variables$bat, variables$velc, variables$sst, variables$d_cost, variables$d_mar)
-names(predictors2) <- c ('bat', 'velc', 'sst', 'd_cost', 'd_mar')
-predictors2
-
-predictors3 <- stack(variables$bat, variables$velc, variables$d_cost, variables$d_mar, variables$d_traf)
-names(predictors3) <- c ('bat', 'velc', 'd_cost', 'd_mar', 'd_traf')
-predictors3
 
 
 ## Formatando os dados
@@ -183,169 +179,104 @@ myBiomodData1 <- BIOMOD_FormatingData(resp.var = DataSpecies,
                                       resp.xy = myRespXY,
                                       resp.name = myRespName)
 
-myBiomodData2 <- BIOMOD_FormatingData(resp.var = DataSpecies,
-                                      expl.var = predictors2,
-                                      resp.xy = myRespXY,
-                                      resp.name = myRespName)
-
-myBiomodData3 <- BIOMOD_FormatingData(resp.var = DataSpecies,
-                                      expl.var = predictors3,
-                                      resp.xy = myRespXY,
-                                      resp.name = myRespName)
-
 
 ## Definindo opções de modelos usando opções padrão
 myBiomodOption <- BIOMOD_ModelingOptions()
 myBiomodOption
 
+ 
 
 ## Computando os modelos
 myBiomodModelOut1 <- BIOMOD_Modeling(myBiomodData1,
                                      models = c('GLM','RF'), # duas metodologias "generalize linear model", usamos o logistico (minimo 0 e maximo 1)
                                      bm.options = myBiomodOption,
-                                     nb.rep=12,# numero de vezes que o modelo vai sortear a porcentagem determinada pra criar o intervalo de confiança
-                                     data.split.perc=70,# pega só a qttd em porcentagem dos dados pra rodar o modelo # usa o "resto da %" pra fazer a validação (ex: 30%)
-                                     prevalence=0.5, # 50% de ausência e 50% de presença
+                                     CV.strategy = 'random',
+                                     CV.nb.rep = 5,
+                                     CV.perc = 0.7,
                                      var.import=3,# numero de vezes que o modelo faz a analise da importancia da variavel atraves do sorteio
-                                     metric.eval = c('ROC'),# quanto o modelo acerta o 1 e o 0 > capacidade de acertar de fato os verdadeiros 1 e 0 #quanto maior a área da curva, indica que ta acertando bem                                     SaveObj = TRUE,
+                                     metric.eval = c('ROC', 'TSS'),# quanto o modelo acerta o 1 e o 0 > capacidade de acertar de fato os verdadeiros 1 e 0 #quanto maior a área da curva, indica que ta acertando bem                                     SaveObj = TRUE,
                                      scale.models = TRUE,
                                      modeling.id = paste(myRespName,"Model1",sep=""))
+                                     
+
 myBiomodModelOut1
-
-myBiomodModelOut2 <- BIOMOD_Modeling(myBiomodData2,
-                                     models = c('GLM','RF'),
-                                     bm.options = myBiomodOption,
-                                     nb.rep=12,
-                                     data.split.perc=70,
-                                     prevalence=0.5,
-                                     var.import=3,
-                                     metric.eval = c('ROC'),
-                                     scale.models = TRUE,
-                                     modeling.id = paste(myRespName,"Model2",sep=""))
-myBiomodModelOut2
-
-myBiomodModelOut3 <- BIOMOD_Modeling(myBiomodData3,
-                                     models = c('GLM','RF'), #random forest > machine learning
-                                     bm.options = myBiomodOption,
-                                     nb.rep=12,
-                                     data.split.perc=70,
-                                     prevalence=0.5,
-                                     var.import=3,
-                                     metric.eval = c('ROC'),
-                                     scale.models = TRUE,
-                                     modeling.id = paste(myRespName,"Model3",sep=""))
-myBiomodModelOut3
 
 
 ## Obtendo a avaliação de todos os modelos
-myBiomodModelEval1 <- get_evaluations(myBiomodModelOut1)
-myBiomodModelEval2 <- get_evaluations(myBiomodModelOut2)
-myBiomodModelEval3 <- get_evaluations(myBiomodModelOut3)
+get_evaluations(myBiomodModelOut1)
+get_variables_importance(myBiomodModelOut1)
 
 
-## Imprimindo os valores ROC de todos os modelos
-myBiomodModelEval1["ROC","Testing.data","GLM",,]
-meanGLM1 <- mean(myBiomodModelEval1["ROC","Testing.data","GLM",,])
-desvGLM1 <- sd(myBiomodModelEval1["ROC","Testing.data","GLM",,])
-myBiomodModelEval1["ROC","Testing.data","RF",,]
-meanRF1 <- mean(myBiomodModelEval1["ROC","Testing.data","RF",,])
-desvRF1 <- sd(myBiomodModelEval1["ROC","Testing.data","RF",,])
 
-myBiomodModelEval2["ROC","Testing.data","GLM",,]
-meanGLM2 <- mean(myBiomodModelEval2["ROC","Testing.data","GLM",,])
-desvGLM2 <- sd(myBiomodModelEval2["ROC","Testing.data","GLM",,])
-myBiomodModelEval2["ROC","Testing.data","RF",,]
-meanRF2 <- mean(myBiomodModelEval2["ROC","Testing.data","RF",,])
-desvRF2 <- sd(myBiomodModelEval2["ROC","Testing.data","RF",,])
-
-myBiomodModelEval3["ROC","Testing.data","GLM",,]
-meanGLM3 <- mean(myBiomodModelEval3["ROC","Testing.data","GLM",,])
-desvGLM3 <- sd(myBiomodModelEval3["ROC","Testing.data","GLM",,])
-myBiomodModelEval3["ROC","Testing.data","RF",,]
-meanRF3 <- mean(myBiomodModelEval3["ROC","Testing.data","RF",,])
-desvRF3 <- sd(myBiomodModelEval3["ROC","Testing.data","RF",,])
+#Represent evaluation scores & variables importance
+bm_PlotEvalMean(bm.out = myBiomodModelOut1)
+bm_PlotEvalBoxplot(bm.out = myBiomodModelOut1, group.by = c('algo', 'algo'))
+bm_PlotEvalBoxplot(bm.out = myBiomodModelOut1, group.by = c('algo', 'run'))
+bm_PlotVarImpBoxplot(bm.out = myBiomodModelOut1, group.by = c('expl.var', 'algo', 'algo'))
+bm_PlotVarImpBoxplot(bm.out = myBiomodModelOut1, group.by = c('expl.var', 'algo', 'run'))
+bm_PlotVarImpBoxplot(bm.out = myBiomodModelOut1, group.by = c('algo', 'expl.var', 'run'))
 
 
-## Imprimindo os valores de Sensitividade e Especificidade de todos os modelos
-# sensitividade = quanto acerta do 1
-# especificidade = quanto acerta do 0
-# matriz de confusao
-myBiomodModelEval1["ROC","Sensitivity","GLM",,]
-meanSenGLM1 <- mean(myBiomodModelEval1["ROC","Sensitivity","GLM",,])
-desvSenGLM1 <- sd(myBiomodModelEval1["ROC","Sensitivity","GLM",,])
-myBiomodModelEval1["ROC","Specificity","GLM",,]
-meanSpeGLM1 <- mean(myBiomodModelEval1["ROC","Specificity","GLM",,])
-desvSpeGLM1 <- sd(myBiomodModelEval1["ROC","Specificity","GLM",,])
-myBiomodModelEval1["ROC","Sensitivity","RF",,]
-meanSenRF1 <- mean(myBiomodModelEval1["ROC","Sensitivity","RF",,])
-desvSenRF1 <- sd(myBiomodModelEval1["ROC","Sensitivity","RF",,])
-myBiomodModelEval1["ROC","Specificity","RF",,]
-meanSpeRF1 <- mean(myBiomodModelEval1["ROC","Specificity","RF",,])
-desvSpeRF1 <- sd(myBiomodModelEval1["ROC","Specificity","RF",,])
+bm_PlotResponseCurves(bm.out = myBiomodModelOut1, 
+                     models.chosen = get_built_models(myBiomodModelOut1)[c(1:3, 12:14)],
+                     fixed.var = 'median')
+bm_PlotResponseCurves(bm.out = myBiomodModelOut1, 
+                      models.chosen = get_built_models(myBiomodModelOut1)[c(1:3, 12:14)],
+                      fixed.var = 'min')
+bm_PlotResponseCurves(bm.out = myBiomodModelOut1, 
+                      models.chosen = get_built_models(myBiomodModelOut1)[2],
+                      fixed.var = 'median',
+                      do.bivariate = TRUE)
 
-myBiomodModelEval2["ROC","Sensitivity","GLM",,]
-meanSenGLM2 <- mean(myBiomodModelEval2["ROC","Sensitivity","GLM",,])
-desvSenGLM2 <- sd(myBiomodModelEval2["ROC","Sensitivity","GLM",,])
-myBiomodModelEval2["ROC","Specificity","GLM",,]
-meanSpeGLM2 <- mean(myBiomodModelEval2["ROC","Specificity","GLM",,])
-desvSpeGLM2 <- sd(myBiomodModelEval2["ROC","Specificity","GLM",,])
-myBiomodModelEval2["ROC","Sensitivity","RF",,]
-meanSenRF2 <- mean(myBiomodModelEval2["ROC","Sensitivity","RF",,])
-desvSenRF2 <- sd(myBiomodModelEval2["ROC","Sensitivity","RF",,])
-myBiomodModelEval2["ROC","Specificity","RF",,]
-meanSpeRF2 <- mean(myBiomodModelEval2["ROC","Specificity","RF",,])
-desvSpeRF2 <- sd(myBiomodModelEval2["ROC","Specificity","RF",,])
+###### RESOLVER aqui
+# Model ensemble models
+myBiomodEM <- BIOMOD_EnsembleModeling(bm.mod = myBiomodModelOut1,
+                                      models.chosen = 'all',
+                                      em.by = 'all',
+                                      em.algo = c('EMmean', 'EMcv', 'EMci', 'EMmedian', 'EMca', 'EMwmean'),
+                                      metric.select = c('TSS'),
+                                      metric.select.thresh = c(0.7),
+                                      metric.eval = c('TSS','ROC'),
+                                      var.import = 3,
+                                      EMci.alpha = 0.05,
+                                      EMwmean.decay = 'proportional')
+myBiomodEM
 
-myBiomodModelEval3["ROC","Sensitivity","GLM",,]
-meanSenGLM3 <- mean(myBiomodModelEval3["ROC","Sensitivity","GLM",,])
-desvSenGLM3 <- sd(myBiomodModelEval3["ROC","Sensitivity","GLM",,])
-myBiomodModelEval3["ROC","Specificity","GLM",,]
-meanSpeGLM3 <- mean(myBiomodModelEval3["ROC","Specificity","GLM",,])
-desvSpeGLM3 <- sd(myBiomodModelEval3["ROC","Specificity","GLM",,])
-myBiomodModelEval3["ROC","Sensitivity","RF",,]
-meanSenRF3 <- mean(myBiomodModelEval3["ROC","Sensitivity","RF",,])
-desvSenRF3 <- sd(myBiomodModelEval3["ROC","Sensitivity","RF",,])
-myBiomodModelEval3["ROC","Specificity","RF",,]
-meanSpeRF3 <- mean(myBiomodModelEval3["ROC","Specificity","RF",,])
-desvSpeRF3 <- sd(myBiomodModelEval3["ROC","Specificity","RF",,])
+# Get evaluation scores & variables importance
+get_evaluations(myBiomodEM)
+get_variables_importance(myBiomodEM)
+
+# Represent evaluation scores & variables importance
+bm_PlotEvalMean(bm.out = myBiomodEM, group.by = 'full.name')
+bm_PlotEvalBoxplot(bm.out = myBiomodEM, group.by = c('full.name', 'full.name'))
+bm_PlotVarImpBoxplot(bm.out = myBiomodEM, group.by = c('expl.var', 'full.name', 'full.name'))
+bm_PlotVarImpBoxplot(bm.out = myBiomodEM, group.by = c('expl.var', 'algo', 'merged.by.run'))
+bm_PlotVarImpBoxplot(bm.out = myBiomodEM, group.by = c('algo', 'expl.var', 'merged.by.run'))
+
+# Represent response curves
+bm_PlotResponseCurves(bm.out = myBiomodEM, 
+                      models.chosen = get_built_models(myBiomodEM)[c(1, 6, 7)],
+                      fixed.var = 'median')
+bm_PlotResponseCurves(bm.out = myBiomodEM, 
+                      models.chosen = get_built_models(myBiomodEM)[c(1, 6, 7)],
+                      fixed.var = 'min')
+bm_PlotResponseCurves(bm.out = myBiomodEM, 
+                      models.chosen = get_built_models(myBiomodEM)[7],
+                      fixed.var = 'median',
+                      do.bivariate = TRUE)
 
 
-## Obtendo a importância das variáveis para o melhor modelo
-get_variables_importance(myBiomodModelOut3)
-
-varimp_GLM <- get_variables_importance(myBiomodModelOut3)[,"GLM",,]
-varimp_GLM
-varimp_GLM <- t(varimp_GLM)
-write.csv(varimp_GLM, "GLM.csv")
-
-varimp_RF <- get_variables_importance(myBiomodModelOut3)[,"RF",,]
-varimp_RF
-varimp_RF <- t(varimp_RF)
-write.csv(varimp_RF, "RF.csv")
-
-par(mfrow=c(1,2))
-varimp_GLM <- read.table("GLM.csv", header = T, sep = ",", dec = ".")
-boxplot(varimp_GLM$bat, varimp_GLM$velc, varimp_GLM$d_cost, varimp_GLM$d_mar, varimp_GLM$d_traf,
-        names = c('Bat', 'Velc', 'd_Cost', 'd_Mar', 'd_Traf'),
-        ylim=c(0,1),
-        main = "GLM")
-
-varimp_RF <- read.table("RF.csv", header = T, sep = ",", dec = ".")
-boxplot(varimp_RF$bat, varimp_RF$velc, varimp_RF$d_cost, varimp_RF$d_mar, varimp_RF$d_traf,
-        names = c('Bat', 'Velc', 'd_Cost', 'd_Mar', 'd_Traf'),
-        ylim=c(0,1),
-        main = "RF")
 
 
 ## Projetando sobre o globo nas condições atuais
 myBiomodProj <- BIOMOD_Projection(
-  modeling.output = myBiomodModelOut3,
-  new.env = predictors3,
+  bm.mod = myBiomodModelOut1,
+  new.env = predictors1,
   proj.name = 'current',
   selected.models = 'all',
   binary.meth = 'ROC',
   compress = 'xz',
-  clamping.mask = F,
+  build.clamping.mask = FALSE,
   output.format = '.grd')
 myBiomodProj
 
@@ -355,29 +286,6 @@ plot(myBiomodProj)
 plot(myBiomodProj, str.grep = 'GLM')
 plot(myBiomodProj, str.grep = 'RF')
 
-
-## Agrupando os melhores modelos
-myBiomodEM <- BIOMOD_EnsembleModeling(
-  modeling.output = myBiomodModelOut3,
-  chosen.models = 'all',
-  em.by='all',
-  eval.metric = c('ROC'), eval.metric.quality.threshold = c(0.7), prob.mean = T,
-  prob.cv = T,
-  prob.ci = T,
-  prob.ci.alpha = 0.05,
-  prob.median = T,
-  committee.averaging = T,
-  prob.mean.weight = T, prob.mean.weight.decay = 'proportional' )
-myBiomodEM
-
-get_evaluations(myBiomodEM)
-
-
-## Criando as projeções do modelo agrupado
-myBiomodEF <- BIOMOD_EnsembleForecasting(
-  projection.output = myBiomodProj,
-  EM.output = myBiomodEM )
-plot(myBiomodEF)
 
 
 #importing files
